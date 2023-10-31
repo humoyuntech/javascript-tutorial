6f5588e90794b07932637d50a508b091e25ae437
Khumoyun <humoyuntech@gmail.com>
2023-10-31 12:37:34 +0500

    if-else

--- 003_dars/app.js
+++ 003_dars/app.js
@@ -1,1 +1,57 @@
-console.log("hello world");
+// ===============================if else ===============================
+/**
+ let age = prompt("yoshingiz", "");
+console.log(age);
+
+if(age >= 18){
+    alert("Siz Gmail ocha olasiz");
+}else{
+    alert("Siz Gmail ocha olmaysiz");
+}
+*/
+
+let temp = 'isiiqroq';
+
+//====================
+// if (temp == "sovuq") {
+//     console.log("Sovuq");
+// } else if(temp == "iliq") {
+//     console.log("Iliq");
+// }else if(temp == "issiq") {
+//     console.log("Issiq");
+// }else{
+//     console.log("Ma'lumot yo'q");
+// }
+//====================
+
+// ===============================Ternary operator ===============================
+temp = 25;
+// temp > 0 ? console.log("Havo issiq") : console.log("Havo sovuq");
+
+// ===============================Nullish ===============================
+
+console.log(false ?? 2); // false
+console.log(false ?? null); // false
+console.log(null ?? 2); //2
+console.log(2 ?? null); //2
+console.log(undefined ?? null); //null
+console.log(null ?? undefined); // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// condition
