30bd8d59633ed5443e16246e30f945992fab6066
Khumoyun <humoyuntech@gmail.com>
2023-11-01 03:01:43 +0500

    Swtich || For Loop

---
+++ 004_dars/app.js
@@ -0,0 +1,84 @@
+let data = "yomg'ir"
+
+// switch(data) {
+//     case "yomg'ir":
+//         console.log("Bugun yomg'ir");
+//         break;
+//     case "qor":
+//         console.log("Bugun qor");
+//     case "tuman":
+//         console.log("Bugun tuman");
+//         break;
+//     default:
+//         console.log("Ob-xavo xaqda ma'lumot yo'q");
+// }
+
+
+//==============================================For Loop==============================================
+
+// for(let i = 0; i <= data.length; i++) {
+//     console.log(data[i]);
+// }
+
+// console.log(i);
+
+//=======================break and continue=======================
+/**
+ * 
+for(let i = 1; i <= 10; i++) {
+    if(i===6){
+        //break; //to'xtadi
+       continue; // tashlab ketadi
+    }
+    console.log(i);
+}
+ */
+
+
+
+
+// ------------juft va toq sonlarni chiqarish --- output even and odd numbers
+/**
+ * 
+let even = []; //juft
+let odd = []; //toq
+
+for(let i = 1; i <= 10; i++) {
+    if(i%2 == 0) {
+        even.push(i)
+    }else{
+        odd.push(i)
+    }
+}
+
+console.log(even);
+console.log(odd);
+
+ */
+// ------------------------------------
+
+// for(i = 10; i>= 1; i--) {
+//     for(j = 1; j <= 10; j++) {
+//         console.log(i);
+//     }
+// }
+/**
+ * yuqoridagi holatga console.log(i) bo'lsa har bitta
+ *son 10 martadan chiqai
+ * 10-1 ga qaytadi
+ */
+
+// ------------------------------------
+for(let i = 1; i <= 10; i++){
+    console.log(`9 * ${i} =`, i * 9);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 004_dars/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for(let i = 10; i >= 10; i++){
+    console.log(i);
+}
+
