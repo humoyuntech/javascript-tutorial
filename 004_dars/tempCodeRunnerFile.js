f447c9076d65bdc74b9366dd52ca9a23af95d418
Khumoyun <humoyuntech@gmail.com>
2023-11-02 00:42:35 +0500

    while | do while | functions

--- 004_dars/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-for(let i = 10; i >= 10; i++){
-    console.log(i);
-}
-
---
+++ 005_dars/app.js
@@ -0,0 +1,64 @@
+// =========== The “while” loop ===========
+
+// -------------------
+/*
+let num = 0;
+while(num <= 5) {
+    console.log(num);
+    num++;
+}
+*/
+// =========== The “do…while” loop ===========
+/*
+let i = 0;
+do {
+  console.log(i);( i );
+  i++;
+} while (i < 3);
+*/
+
+
+//=======================================Function=======================================
+//================================= Function Declaration
+// function getAvr() {
+//   console.log("Hello my friend");
+// }
+// getAvr();
+
+//=================================Function expressions
+/*
+function sayHi() {   // (1) create
+  console.log( "Hello" );
+}
+
+let func = sayHi;    // (2) copy
+
+func(); // Hello     // (3) run the copy (it works)!
+sayHi(); // Hello    //     this still works too (why wouldn't it)
+
+*/
+//=================================Arrow functions
+/*
+const getAvrArrow =()=>{
+  console.log("Hello Arrow");
+}
+getAvrArrow();
+*/
+
+
+//=================================Function parameter argument
+
+/*
+function myFunc(parameter1, parameter2) {
+  body
+}
+
+ myFunc(argument1, argument2)
+ */
+
+
+
+
+
+
+
