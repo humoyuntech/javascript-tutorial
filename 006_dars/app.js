c94a27b2b86c6b9764ccab03b7e1a050e1bed93f
Khumoyun <humoyuntech@gmail.com>
2023-11-11 23:33:23 +0500

    data

---
+++ 006_dars/app.js
@@ -0,0 +1,15 @@
+
+let data = "yomg'ir"
+
+switch(data) {
+    case "yomg'ir":
+        console.log("Bugun yomg'ir");
+        break;
+    case "qor":
+        console.log("Bugun qor");
+    case "tuman":
+        console.log("Bugun tuman");
+        break;
+    default:
+        console.log("Ob-xavo xaqda ma'lumot yo'q");
+}
