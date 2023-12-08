2698e887806bbdc3180ae7f7118ce37d3f18937a
Khumoyun <humoyuntech@gmail.com>
2023-12-08 16:09:52 +0500

    Cache, DDOS attack Types

---
+++ 020_dars/app.js
@@ -0,0 +1,45 @@
+console.log("DDOS-attack");
+
+function getData() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+    res.json()
+  );
+}
+
+function getFullname() {
+    return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json()
+    );
+  }
+  
+function getCached(func) {
+    let cache = new Map();
+    return function(x) {
+        if(cache.has(x)) {
+            console.log(cache.get(x));
+            return cache.get(x);
+        }
+        let res = func();
+        return cache.set(x, res);
+    };
+};
+
+getData = getCached(getData)
+getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
