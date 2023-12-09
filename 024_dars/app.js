39ca6fa028a76e82b4976fc6059676930d110a81
Khumoyun <humoyuntech@gmail.com>
2023-12-09 19:51:31 +0500

    Classes Advanced

---
+++ 024_dars/app.js
@@ -0,0 +1,44 @@
+/*
+class Webbrain {
+    static year = 0;
+
+    static getYear() {
+        console.log(++this.year);
+    }
+}
+
+Webbrain.getYear(); //1
+Webbrain.getYear(); //2
+Webbrain.getYear(); //3 
+Webbrain.getYear(); //4
+*/
+//===========================================================================================================================================
+/*
+class Protected {
+    _name = "Boss";
+    #protected = "Tegma";
+    getName() {
+        console.log(this.#protected);
+    }
+}
+
+let pr = new Protected();
+console.log(pr.getName());
+*/
+//===========================================================================================================================================
+//===========================================================================================================================================
+// Built-in Clasess
+class Ar extends Array {};
+
+let ar = new Ar();
+
+console.log(ar.length);
+
+
+
+
+
+
+
+
+
