06b64eb923a5791219186494186a1f1dd45d9068
Khumoyun <humoyuntech@gmail.com>
2023-12-10 18:43:02 +0500

    Modules

---
+++ 027_dars/app.js
@@ -0,0 +1,45 @@
+// https://learn.javascript.ru/import-export
+console.log("Moduls");
+import { users } from "./mock.js";
+
+console.log(users);
+// const onDelete = (id) =>{
+//     let res =users.filter((vl) => vl.id !==id);
+//     users = res;
+// };
+
+// onDelete(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 027_dars/mock.js
@@ -0,0 +1,10 @@
+export let users = [
+    {id: 1, name: "Khumoyun1"},
+    {id: 2, name: "Khumoyun2"},
+    {id: 3, name: "Khumoyun3"},
+    {id: 4, name: "Khumoyun4"},
+    {id: 5, name: "Khumoyun5"},
+    {id: 6, name: "Khumoyun6"},
+];
+
+export const list = [1, 2, 3];
