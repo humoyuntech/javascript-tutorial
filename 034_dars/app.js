e60b7c94d233d312913e2236199d14ce2720ce85
Khumoyun <humoyuntech@gmail.com>
2024-01-24 15:26:03 +0500

    Form

---
+++ 034_dars/app.js
@@ -0,0 +1,20 @@
+"use strict";
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(two.value);
+    console.log(tree.value);
+    console.log(select.value); // optionni valuesini chiqaradi
+    console.log(select.selectedIndex); //selectni indexni chiqaradi
+})
+
+
+
+
+
+
+
+
+
+
